495c3043e778152f91c531f4408fa98604c2820d
a-givertzman <a.givertzman@icloud.com>
2023-11-27 18:23:25 +0300

    export table schema

--- lib/dart_api_client.dart
+++ lib/dart_api_client.dart
@@ -6,3 +6,10 @@ export 'src/api_client/query/sql_query.dart';
 export 'src/api_client/query/python_query.dart';
 export 'src/api_client/reply/api_reply.dart';
 export 'src/api_client/request/api_request.dart';
+
+export 'src/sql/sql.dart';
+export 'src/table_schema/field.dart';
+export 'src/table_schema/field_value.dart';
+export 'src/table_schema/relation.dart';
+export 'src/table_schema/schema.dart';
+export 'src/table_schema/schema_entry.dart';
