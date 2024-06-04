b7e44c9501477d574e758895b2ceccc6f74fad09
nyaneet <nyaneet@gmail.com>
2024-06-04 15:37:08 +0300

    fix: add missing export of ExecutableQuery

--- lib/ext_rw.dart
+++ lib/ext_rw.dart
@@ -3,6 +3,7 @@ library ext_rwcore;
 export 'src/api_client/address/api_address.dart';
 export 'src/api_client/query/api_query_type.dart';
 export 'src/api_client/query/sql_query.dart';
+export 'src/api_client/query/executable_query.dart';
 export 'src/api_client/query/python_query.dart';
 export 'src/api_client/reply/api_reply.dart';
 export 'src/api_client/request/api_request.dart';
